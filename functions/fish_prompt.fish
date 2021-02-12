e4fba710cca177abe3f18dbe1a5dbb140a8df1b8
Angad Singh <mail@angad.dev>
2021-02-12 13:36:23 +0530

    New prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -5,9 +5,9 @@ function fish_prompt --description 'Write out the prompt'
     set -l suffix
     switch $fish_bind_mode
       case default
-        set suffix '<'
+        set suffix '<<'
       case insert
-        set suffix '>'
+        set suffix '>>'
       case replace_one
         set suffix '^'
       case visual
@@ -16,9 +16,11 @@ function fish_prompt --description 'Write out the prompt'
         echo '?'
     end
 
-    echo -n -s (set_color 608cc3) (prompt_pwd) (set_color normal) \
+    echo -n -s "λ" \
+        (set_color --bold ff0000) " :: " (set_color normal) \
+        (set_color --bold 00ff00) (prompt_pwd) (set_color normal) \
         (fish_vcs_prompt) \
         (__fish_print_pipestatus " [" "]" "|" (set_color $fish_color_status) \
-        (set_color --bold $fish_color_status) $last_pipestatus) \
-        (set_color normal) "$suffix "
+        (set_color --bold $fish_color_status) $last_pipestatus) " "\
+        (set_color --bold 2977f4) "$suffix " (set_color normal)
 end
